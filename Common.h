697486ee6f03ed8638d2dec33614946f4ad9e39a
PoomSmart <PoomSmart@Mac-Pro.local>
2014-10-01 19:33:05 +0700

    Flipswitch capability.

---
+++ Common.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+
+#define kSpringBoard CFSTR("com.apple.springboard")
+#define kBlurBarEnabledKey CFSTR("SBBlurryStatusBarEnabled")
+#define kBlurBarStyle CFSTR("SBBlurryStatusBarStyle")
+#define kNotifyKey "com.insanj.blurbar.pref"
